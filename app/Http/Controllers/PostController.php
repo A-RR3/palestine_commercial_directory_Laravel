1ab47e8f8954fe4483db566ee1495b0ea62fee2b
Aseel-Mohtaseb <aseelamermohtaseb@gmail.com>
2024-05-29 16:09:50 +0300

    test

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,9 +2,41 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class PostController extends Controller
 {
-    //
+    public function uploadVideoTest(Request $request)
+    {
+        $validator = Validator::make($request->all(), []);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'status' => false,
+                'message' => 'error',
+            ]);
+        }
+
+        $post = new Post();
+        $post->p_title = 'test';
+        $post->p_content = 'test';
+        $post->p_user_id = 2;
+
+        // upload video
+
+        if ($post->save()) {
+            return response()->json([
+                'status' => true,
+                'message' => 'done',
+                'post' => $post,
+            ]);
+        }
+
+        return response()->json([
+            'status' => false,
+            'message' => 'error in adding',
+        ]);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\PostController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,8 +17,10 @@ use App\Http\Controllers\AuthController;
 */
 
 Route::group(['middleware' => ['auth:sanctum']],function () {
-  
+
 });
 
-Route::post('/auth/register',[AuthController::class,'register']);
-Route::post('/auth/login',[AuthController::class,'loginUser']);
+// Route::post('/auth/register',[AuthController::class,'register']);
+// Route::post('/auth/login',[AuthController::class,'loginUser']);
+
+Route::post('/post', [PostController::class, 'uploadVideoTest']);
