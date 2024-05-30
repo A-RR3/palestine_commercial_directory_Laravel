ad363aa6434f83009a5e021f19d460b0b4cf33df
Aseel-Mohtaseb <aseelamermohtaseb@gmail.com>
2024-05-30 15:30:24 +0300

    add video post

---
+++ app/Services/FileUploadService.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Services;
+
+use Illuminate\Http\UploadedFile;
+
+class FileUploadService
+{
+    /**
+     * Handle the file upload.
+     *
+     * @param UploadedFile $file
+     * @param string $folderPath
+     * @param string $disk
+     * @return string The file path
+     */
+    public function uploadVideo(UploadedFile $file, $folderPath, $disk = 'public')
+    {
+        $extension = $file->getClientOriginalExtension();
+        $fileName = time() . '_' . uniqid() . '.' . $extension;
+        $file->storeAs($folderPath, $fileName, $disk);
+
+        return $fileName;
+    }
+}
